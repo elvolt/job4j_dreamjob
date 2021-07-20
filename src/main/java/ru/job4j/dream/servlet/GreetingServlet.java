878701047e08b3ea19ee49f0859a12af7eb87e2c
elvolt <sidelevcs@gmail.com>
2021-07-20 21:12:44 +0300

    4. Ajax Jquery [#58534]

---
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -0,0 +1,21 @@
+package ru.job4j.dream.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GreetingServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+        String email = req.getParameter("email");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.println("Your email: " + email);
+        writer.flush();
+    }
+}
